9e120298812c74288da038221003b6279fffea9e
xpercy29 <spangeled@gmail.com>
2024-07-10 02:27:58 +0530

    ejs installed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejs = require('ejs');
 
 const http = require("http");
 
--- public/js/script.js
+++ public/js/script.js
@@ -17,4 +17,8 @@ if (navigator.geolocation) {
   );
 }
 
-L.map("map").setView[0,];
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap",
+}).addTo(map);
